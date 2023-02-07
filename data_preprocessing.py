a16036092670ca9235e15348cf6ba10a16c7d8b9
ndesai86 <ndesai86@gmail.com>
2023-02-07 17:51:34 +0100

    Added a new function sum

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,3 +2,12 @@
 print(" All the code for data preprocessing will be entered here")
 
 print(" Addtional code to be entered")
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return a
+
+print("Added a new function sum which is incorrect")
